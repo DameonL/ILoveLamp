eeeb8c0a6fd63338dee3b70abaa0f9b51b0d616f
Dameon Laird <dameonlair@rocketmail.com>
2022-03-14 16:54:13 -0700

    Add files via upload

--- src/ShopList.js
+++ src/ShopList.js
@@ -142,7 +142,7 @@ class ShopList extends HTMLElement {
 
     renderPageButtons() {
         let pageChanged = () => {
-            if (this.#startIndex >= this.#productCount - this.#productsPerPage - 1) this.#startIndex = this.#productCount - this.#productsPerPage - 1;
+            if (this.#startIndex >= this.#productCount - this.#productsPerPage) this.#startIndex = this.#productCount - this.#productsPerPage;
             if (this.#startIndex < 0) this.#startIndex = 0;
 
             let params = new URLSearchParams(window.location.hash.substring(1));
