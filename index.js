0190da7ee5fcfdcb04eef9880a813780b7587b1d
Dameon Laird <dameonlair@rocketmail.com>
2022-03-16 16:57:12 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import ILoveLampNavbar from "../src/ILoveLampNavbar.js";
 import ShopList from "../src/ShopList.js";
 import ProductView from "../src/ProductView.js";
 import ShoppingCart from "./src/ShoppingCart.js";
-customElements.define('shop-list', ShopList, );
+customElements.define('shop-list', ShopList,);
 customElements.define('product-view', ProductView);
 customElements.define('shopping-cart', ShoppingCart);
 customElements.define('ilovelamp-navbar', ILoveLampNavbar);
@@ -13,9 +13,9 @@ let shopName = document.head.querySelector("title").innerText;
 window.addEventListener("hashchange", hashChanged);
 window.addEventListener("popstate", hashChanged);
 let pageIds = {
-    "" : "pages/index.html",
-    "Shop" : "pages/ShopList.html",
-    "About" : "pages/About.html"
+    "": "pages/index.html",
+    "Shop": "pages/ShopList.html",
+    "About": "pages/About.html"
 }
 
 function hashChanged() {
@@ -35,9 +35,21 @@ let loadPage = async (pageId = "") => {
         url = pageIds[pageId];
     }
 
+    var setInnerHTML = function (elm, html) {
+        elm.innerHTML = html;
+        Array.from(elm.querySelectorAll("script")).forEach(oldScript => {
+            const newScript = document.createElement("script");
+            Array.from(oldScript.attributes)
+                .forEach(attr => newScript.setAttribute(attr.name, attr.value));
+            newScript.appendChild(document.createTextNode(oldScript.innerHTML));
+            oldScript.parentNode.replaceChild(newScript, oldScript);
+        });
+    }
+
     let navbarTarget = document.createElement("div");
     let pageHtml = await fetch(url);
-    pageRenderer.innerHTML = await pageHtml.text();
+    pageHtml = await pageHtml.text();
+    setInnerHTML(pageRenderer, pageHtml);
     if (pageRenderer.querySelector("title")) {
         document.title = `${shopName} - ${pageRenderer.querySelector("title").innerText}`;
     } else {
