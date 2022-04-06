8b6d1ca68865d1e6d78bdc46ea392acca77db317
Dameon Laird <dameonlair@rocketmail.com>
2022-04-06 16:53:40 -0700

    Add files via upload

--- src/ContactPage.js
+++ src/ContactPage.js
@@ -3,11 +3,40 @@ import FetchHtmlElement from "./FetchHtmlElement.js";
 class ContactPage extends FetchHtmlElement {
     constructor() {
         super();
-        this.addHtmlLoadedHandler(() => this.#pageLoaded());
+        this.addHtmlLoadedHandler(() => { this.#pageLoaded(); });
     }
 
     async #pageLoaded() {
-        let submitButton = this.getElementById("submitButton");
+        let submitButton = this.querySelector("#submitButton");
+        submitButton.addEventListener("click", () => {
+            if (!this.#validateForm()) {
+                alert("Please complete all fields before submitting.");
+                return;
+            }
+
+            submitButton.disabled = true;
+            this.querySelector(`[name="name"]`).disabled = true;
+            this.querySelector(`[name="email"]`).disabled = true;
+            this.querySelector(`[name="topic"]`).disabled = true;
+            this.querySelector(`[name="message"]`).disabled = true;
+            this.querySelector(`.submitButtonSpinner`).style.display = "block";
+
+            setTimeout(() => {
+                this.innerHtml = "";
+                this.innerText = "Your message has been sent, thank you very much!";
+            }, 5000);
+        });
+    }
+
+    #validateForm() {
+        let isValid = true;
+
+        if (!this.querySelector(`[name="name"]:valid`)) isValid = false;
+        if (!this.querySelector(`[name="email"]:valid`)) isValid = false;
+        if (!this.querySelector(`[name="topic"]:valid`)) isValid = false;
+        if (!this.querySelector(`[name="message"]:valid`)) isValid = false;
+
+        return isValid;
     }
 }
 
